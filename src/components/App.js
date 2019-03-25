d7d3648667b11447ca0d5e844459fea38b27f974
RobertS <rshugart@protonmail.com>
2019-03-25 15:03:58 -0400

    Created  a multi line JSX expression in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PostList from "./PostList";
 
 const App = () => {
-  return <div className="ui containter">App</div>;
+  return (
+    <div className="ui containter">
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
