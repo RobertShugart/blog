7a93ccfef014a89758fee7d787b07c3bd6e9a1d4
RobertS <rshugart@protonmail.com>
2019-04-03 16:38:29 -0400

    Changed payload in actions/index.js  to only  have the data key in it.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async disptatch => {
   const response = await jsonPlaceholder.get("/post");
 
-  dispatchEvent({ type: "FETCH_POSTS", payload: response });
+  dispatchEvent({ type: "FETCH_POSTS", payload: response.data });
 };
