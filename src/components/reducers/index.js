db49844b1f1b0db42a641492bc714e53648d9d5f
RobertS <rshugart@protonmail.com>
2019-04-03 16:09:17 -0400

    Created postsReducer

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  replaceMe: () => "hi there, replace when we know what reducers we need."
+  post: postsReducer
 });
