25799fc761940946e5a56a1615530acae0e1184c
RobertShugart <38505696+RobertShugart@users.noreply.github.com>
2019-04-17 14:22:01 -0400

    Finished/Used lodash to pull users id once in PostList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //Using lodash to get an uniq array of all userId's
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
